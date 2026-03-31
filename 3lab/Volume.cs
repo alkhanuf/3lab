b7fac03e119048561f7fa9ee1583286114f282f9
alkhanuf <simoon.phv@gmail.com>
2026-03-31 19:39:18 +0800

    создание структуры класса Объем и первые тесты

---
+++ 3lab/Volume.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3lab
+{
+    public enum VolumeType { m3, ml, l, barr };
+    public class Volume
+    {
+        private double value;
+        private VolumeType type;
+
+        public Volume(double value, VolumeType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case VolumeType.m3:
+                    typeVerbose = "м3";
+                    break;
+                case VolumeType.ml:
+                    typeVerbose = "мл";
+                    break;
+                case VolumeType.l:
+                    typeVerbose = "л";
+                    break;
+                case VolumeType.barr:
+                    typeVerbose = "барр";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
+    }
+}
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,24 @@
+﻿using _3lab;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void VerboseTest()
+        {
+            var volume1 = new Volume(10, VolumeType.m3);
+            Assert.AreEqual("10 м3", volume1.Verbose());
+
+            var volume2 = new Volume(200, VolumeType.ml);
+            Assert.AreEqual("200 мл", volume2.Verbose());
+
+            var volume3 = new Volume(10, VolumeType.l);
+            Assert.AreEqual("10 л", volume3.Verbose());
+
+            var volume4 = new Volume(10, VolumeType.barr);
+            Assert.AreEqual("10 барр", volume4.Verbose());
+        }
+    }
+}
