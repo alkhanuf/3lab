58992a637406a4dc7bec78c08bfa6f6058b774e8
alkhanuf <simoon.phv@gmail.com>
2026-03-31 21:29:11 +0800

    GUI и операции +- в нем

--- 3lab/Form1.Designer.cs
+++ 3lab/Form1.Designer.cs
@@ -28,12 +28,99 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            cmbOperation = new ComboBox();
+            txtFirst = new TextBox();
+            txtSecond = new TextBox();
+            txtResult = new TextBox();
+            cmbFirstType = new ComboBox();
+            cmbSecondType = new ComboBox();
+            cmbResultType = new ComboBox();
+            SuspendLayout();
+            // 
+            // cmbOperation
+            // 
+            cmbOperation.FormattingEnabled = true;
+            cmbOperation.Items.AddRange(new object[] { "+", "-" });
+            cmbOperation.Location = new Point(62, 88);
+            cmbOperation.Name = "cmbOperation";
+            cmbOperation.Size = new Size(68, 28);
+            cmbOperation.TabIndex = 0;
+            cmbOperation.Text = "+";
+            cmbOperation.SelectedIndexChanged += cmbOperation_SelectedIndexChanged;
+            // 
+            // txtFirst
+            // 
+            txtFirst.Location = new Point(168, 66);
+            txtFirst.Name = "txtFirst";
+            txtFirst.Size = new Size(125, 27);
+            txtFirst.TabIndex = 1;
+            txtFirst.TextChanged += txtFirst_TextChanged;
+            // 
+            // txtSecond
+            // 
+            txtSecond.Location = new Point(168, 112);
+            txtSecond.Name = "txtSecond";
+            txtSecond.Size = new Size(125, 27);
+            txtSecond.TabIndex = 2;
+            txtSecond.TextChanged += txtSecond_TextChanged;
+            // 
+            // txtResult
+            // 
+            txtResult.Location = new Point(62, 160);
+            txtResult.Name = "txtResult";
+            txtResult.Size = new Size(231, 27);
+            txtResult.TabIndex = 3;
+            // 
+            // cmbFirstType
+            // 
+            cmbFirstType.FormattingEnabled = true;
+            cmbFirstType.Location = new Point(310, 66);
+            cmbFirstType.Name = "cmbFirstType";
+            cmbFirstType.Size = new Size(45, 28);
+            cmbFirstType.TabIndex = 4;
+            // 
+            // cmbSecondType
+            // 
+            cmbSecondType.FormattingEnabled = true;
+            cmbSecondType.Location = new Point(310, 112);
+            cmbSecondType.Name = "cmbSecondType";
+            cmbSecondType.Size = new Size(45, 28);
+            cmbSecondType.TabIndex = 5;
+            // 
+            // cmbResultType
+            // 
+            cmbResultType.FormattingEnabled = true;
+            cmbResultType.Location = new Point(310, 159);
+            cmbResultType.Name = "cmbResultType";
+            cmbResultType.Size = new Size(45, 28);
+            cmbResultType.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(383, 242);
+            Controls.Add(cmbResultType);
+            Controls.Add(cmbSecondType);
+            Controls.Add(cmbFirstType);
+            Controls.Add(txtResult);
+            Controls.Add(txtSecond);
+            Controls.Add(txtFirst);
+            Controls.Add(cmbOperation);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox cmbOperation;
+        private TextBox txtFirst;
+        private TextBox txtSecond;
+        private TextBox txtResult;
+        private ComboBox cmbFirstType;
+        private ComboBox cmbSecondType;
+        private ComboBox cmbResultType;
     }
 }
--- 3lab/Form1.cs
+++ 3lab/Form1.cs
@@ -5,6 +5,66 @@ namespace _3lab
         public Form1()
         {
             InitializeComponent();
+
+            var volumeTypes = new string[]
+            {
+                "м3",
+                "мл",
+                "л",
+                "б"
+            };
+
+            cmbFirstType.DataSource = new List<string>(volumeTypes);
+            cmbSecondType.DataSource = new List<string>(volumeTypes);
+            cmbResultType.DataSource = new List<string>(volumeTypes);
+        }
+
+        private void Calculate()
+        {
+            try
+            {
+                var firstValue = double.Parse(txtFirst.Text);
+                var secondValue = double.Parse(txtSecond.Text);
+
+                var firstVolume = new Volume(firstValue, VolumeType.m3);
+                var secondVolume = new Volume(secondValue, VolumeType.m3);
+
+                Volume sumVolume;
+
+                switch (cmbOperation.Text)
+                {
+                    case "+":
+                        sumVolume = firstVolume + secondVolume;
+                        break;
+                    case "-":
+                        sumVolume = firstVolume - secondVolume;
+                        break;
+                    default:
+                        sumVolume = new Volume(0, VolumeType.m3);
+                        break;
+                }
+
+                txtResult.Text = sumVolume.Verbose();
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void txtFirst_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+        private void cmbOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Calculate();
         }
     }
 }
